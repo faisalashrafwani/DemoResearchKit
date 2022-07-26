7236b69790de40b6e090ceab9d73cb30e48c6415
faisalashrafwani <faisalashrafwani@gmail.com>
2022-07-26 14:35:54 +0530

    working json and timestamps

--- DemoResearchKitApp/ViewController.swift
+++ DemoResearchKitApp/ViewController.swift
@@ -9,11 +9,14 @@ import UIKit
 import ResearchKit
 
 class ViewController: UIViewController {
-    var captureDate: String = "##########"
+//    var captureDateLocal: String = "##########"
+//    var captureDateUTC: String = "##########"
     var nameVar: String = "##########"
     var qualificationVar: String = "##########"
     var adventureTypeVar: String = "##########"
     var signatureTypeVar: String = "##########"
+    
+    var id: Int = 1
     
     var jsonArrayTop: [Any] = []
     var jsonDict: [String : Any] = [:]
@@ -22,6 +25,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 //        // Do any additional setup after loading the view.
+        
 //        let testImg = UIImage(named: "baloonride")
 //        let imageDatum = testImg?.pngData()!
 //        let chkImg = imageDatum?.base64EncodedString(options: Data.Base64EncodingOptions.lineLength64Characters)
@@ -30,21 +34,29 @@ class ViewController: UIViewController {
         
     }
     
-    func dateTimeCapture() -> String{
+    func dateTimeCaptureLocal() -> String {
+        
         let formatter = DateFormatter()
-        formatter.amSymbol = "am"
-        formatter.pmSymbol = "PM"
-        formatter.dateStyle = .full
-        formatter.timeStyle = .full
-        formatter.locale = Locale(identifier: "en_IN")
-        formatter.dateFormat = "EEEE, MM/dd/yyyy, h:mm:ss a"
+        formatter.timeZone = .current
+        formatter.locale = .current
+        formatter.dateFormat = "yyyy-MM-dd'T'HH: mm: ssZ"
         return (formatter.string(from: Date()))
+    }
+    func dateTimeCaptureUTC() -> String {
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd'T'HH: mm: ssZ"
+        dateFormatter.timeZone = TimeZone(identifier: "UTC")
+        dateFormatter.locale = Locale(identifier: "UTC")
+        return (dateFormatter.string(from: Date()))
     }
     
     
     @IBAction func surveyTapped(_ sender: Any) {
         //Capturing date time on tapping survey button.
-        captureDate = dateTimeCapture()
+//        captureDateLocal = dateTimeCaptureLocal()
+//        captureDateUTC = dateTimeCaptureUTC()
+        
 
         let taskViewController = ORKTaskViewController(task: SurveyTask, taskRun: nil)
           taskViewController.delegate = self
@@ -62,7 +74,7 @@ extension ViewController: ORKTaskViewControllerDelegate {
         case .saved, .completed:
             
             let taskResults = taskViewController.result.results
-            jsonDict["dateTimeCapture"] = captureDate
+            //jsonDict["dateTimeCapture"] = captureDate
             
             for stepResults in taskResults! as! [ORKStepResult]{
                 for result in stepResults.results! {
@@ -71,10 +83,16 @@ extension ViewController: ORKTaskViewControllerDelegate {
                     if result.identifier == "QuestionStep" {
                         let nameResult = result as? ORKQuestionResult
                         if let user = nameResult?.answer {
+                            jsonDict["id"] = id
                             jsonDict["name"] = user
+                            id += 1
                         } else {
+                            jsonDict["id"] = id
                             jsonDict["name"] = nameVar
+                            id += 1
                         }
+                        jsonArrayTop.append(jsonDict)
+                        jsonDict = [:]
                     }
                     
                     //QUALIFICATION STEP
@@ -85,18 +103,28 @@ extension ViewController: ORKTaskViewControllerDelegate {
                             let qualification = x.first!
                             
                             if qualification == 0 {
+                                jsonDict["id"] = id
                                 jsonDict["qualification"] = "Diploma"
+                                id += 1
                             }
                             else if qualification == 1 {
+                                jsonDict["id"] = id
                                 jsonDict["qualification"] = "Graduation"
+                                id += 1
                             }
                             else {
+                                jsonDict["id"] = id
                                 jsonDict["qualification"] = "Post Graduation"
+                                id += 1
                             }
                             
                         } else {
+                            jsonDict["id"] = id
                             jsonDict["qualification"] = qualificationVar
+                            id += 1
                         }
+                        jsonArrayTop.append(jsonDict)
+                        jsonDict = [:]
                     }
                     
                     //IMAGE STEP
@@ -105,10 +133,18 @@ extension ViewController: ORKTaskViewControllerDelegate {
                         if let choiceImage = imageChoiceResult?.answer {
                             let x = choiceImage as! [String]
                             let choice = x.first!
+
+                            jsonDict["id"] = id
                             jsonDict["adventureType"] = choice
+                            id += 1
+                            
                         } else {
+                            jsonDict["id"] = id
                             jsonDict["adventureType"] = adventureTypeVar
+                            id += 1
                         }
+                        jsonArrayTop.append(jsonDict)
+                        jsonDict = [:]
                     }
                     
                     //SIGNATURE STEP
@@ -124,6 +160,8 @@ extension ViewController: ORKTaskViewControllerDelegate {
                             jsonDict["signature"] = signatureTypeVar
                         }
                     }
+                    
+                    
 //                    if result.identifier == "AudioRecordStepInSurvey" {
 //                        let audioResult = result as? ORKAudioRecorder
 //                        if let audioRecord = audioResult?.audioRecorder{
@@ -137,12 +175,15 @@ extension ViewController: ORKTaskViewControllerDelegate {
                         
                 }
             }
-                jsonArrayTop.append(jsonDict)
             
+            jsonDict["id"] = id; id += 1
+            jsonDict["timeCaptureLocal"] = dateTimeCaptureLocal()
+            jsonDict["timeCaptureUTC"] = dateTimeCaptureUTC()
+            jsonArrayTop.append(jsonDict)
+            jsonDict = [:]
+            id = 1
             
-            
-            
-            
+            //CREATION OF JSON FILE
             do {
                 let jsonData = try JSONSerialization.data(withJSONObject: jsonArrayTop, options: .prettyPrinted)
                 
